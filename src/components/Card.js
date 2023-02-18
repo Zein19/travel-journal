6cc0ead953202b0f9e1b8617eb43095e1020eb7b
Zein19 <zein.23@hotmail.co.uk>
2023-02-18 19:33:11 +0000

    added styling and more data

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,14 @@
 function Card(props){
     return(
         <div className="card">
-            <img className="image" src={props.item.imageUrl} alt={props.item.title} />
-            <div className="card-info">
-                <h3><span>{props.item.location.toUpperCase()}</span> <a href={props.item.googleMapsUrl} target="_blank" rel="noreferrer">View on Google Maps</a></h3>
-                <h1 className="card-title">{props.item.title}</h1>
-                <h4 className="card-date"> {props.item.startDate} - {props.item.endDate}</h4>
-                <p> {props.item.description}</p>   
+           <div className="flex">
+                <img className="image" src={props.item.imageUrl} alt={props.item.title} />
+                <div className="card-info">
+                    <h3><span>{props.item.location.toUpperCase()}</span> <br></br> <a href={props.item.googleMapsUrl} target="_blank" rel="noreferrer">View on Google Maps</a></h3>
+                    <h1 className="card-title">{props.item.title}</h1>
+                    <h4 className="card-date"> {props.item.startDate} - {props.item.endDate}</h4>
+                    <p> {props.item.description}</p>   
+                </div>
             </div>
             <hr />
         </div>
